52e5cefdbc1e0d39ca79a536064652ea4b4d96af
newton-c <cnewton2@buffalo.edu>
2020-12-17 23:21:35 -0500

    additional testing

--- test_script.R
+++ test_script.R
@@ -1,6 +1,5 @@
-#devtools::install_github("newton-c/merge_stats_R")
-#library(merge.stats)
-source("R/merge_stats.R")
+devtools::install_github("newton-c/merge_stats_R")
+library(merge.stats)
 
 dat.1 <- read.csv("~/Desktop/code_translations_r_package/ucdp-dyadic-201.csv")
 dat.2 <- read.csv("~/Desktop/code_translations_r_package/ucdp-prio-acd-201.csv")
