d0d5c5913025799dbaaadbffb9e41e12bcbb574e
newton-c <cnewton2@buffalo.edu>
2020-12-17 23:19:45 -0500

    adding warnings

--- R/merge_stats.R
+++ R/merge_stats.R
@@ -4,48 +4,74 @@ merge_stats <- function(x, y, by = intersect(names(x), names(y)), by.x = by,
     by.y = by, all = FALSE, all.x = all, all.y = all, sort = TRUE,
     suffixes = c(".x", ".y"), no.dups = TRUE, incomparables = NULL,
     show.stats = TRUE, ...) {
-        x$merge <- 1
-        y$merge <- 1
-        m.df <- merge(x, y, by = intersect(names(x), names(y)), by.x = by,
-              by.y = by, all = FALSE, all.x = all, all.y = all, sort = TRUE,
-              suffixes = c(".x", ".y"), no.dups = TRUE, incomparables = NULL,
-              ...)
-        m.df$merge <- NA
-       for (i in 1:nrow(m.df)) {
-               if (m.df$merge.x[i] == 1 & m.df$merge.y[i] != 1) {
-                      m.df$merge[i] <- 1
-               } else if (m.df$merge.x[i] != 1 & m.df$merge.y[i] == 1) {
-                      m.df$merge[i] <- 2
-               } else if (m.df$merge.x[i] == 1 & m.df$merge.y[i] == 1) {
-                      m.df$merge[i] <- 3
-               } else {
-                      m.df$merge[i]  <- NA
+        x.name <- deparse(substitute(x))
+        y.name <- deparse(substitute(y))
+        if ("merge" %in% colnames(x)) {
+            return(cat(paste("\n", "Error: variable 'merge' already exists in",
+                  x.name, "\n", sep = " ")))
+        } else if ("merge" %in% colnames(y)) {
+            return(cat(paste("\n", "Error: variable 'merge' already exists in",
+                  y.name, "\n", sep = " ")))
+        } else if ("merge.x" %in% colnames(x)) {
+            return(cat(paste("\n",
+                   "Error: variable 'merge.x' already exists in",
+                  x.name, "\n", sep = " ")))
+        } else if ("merge.x" %in% colnames(y)) {
+            return(cat(paste("\n",
+                   "Error: variable 'merge.x' already exists in", y.name,
+                   "\n", sep = " ")))
+        } else if ("merge.y" %in% colnames(x)) {
+            return(cat(paste("\n",
+                   "Error: variable 'merge.y' already exists in", x.name,
+                   "\n", sep = " ")))
+        } else if ("merge.y" %in% colnames(y)) {
+            return(cat(paste("\n",
+                   "Error: variable 'merge.y' already exists in", y.name,
+                   "\n", sep = " ")))
+        } else {
+            x$merge <- 1
+            y$merge <- 1
+            m.df <- merge(x, y, by = intersect(names(x), names(y)), by.x = by,
+                  by.y = by, all = FALSE, all.x = all, all.y = all, sort = TRUE,
+                  suffixes = c(".x", ".y"), no.dups = TRUE,
+                  incomparables = NULL, ...)
+            m.df$merge <- NA
+           for (i in 1:nrow(m.df)) {
+                   if (m.df$merge.x[i] == 1 & m.df$merge.y[i] != 1) {
+                          m.df$merge[i] <- 1
+                   } else if (m.df$merge.x[i] != 1 & m.df$merge.y[i] == 1) {
+                          m.df$merge[i] <- 2
+                   } else if (m.df$merge.x[i] == 1 & m.df$merge.y[i] == 1) {
+                          m.df$merge[i] <- 3
+                   } else {
+                          m.df$merge[i]  <- NA
+                   }
                }
-       }
-       if (show.stats == TRUE) {
-                missing.x <- sum(is.na(m.df$merge) | m.df$merge == 2)
-                sm1 <- paste("Missing from 1st dataframe:",
-                             missing.x, sep = " ")
-                merging.x <- sum(m.df$merge == 1 | m.df$merge == 3,
-                                 na.rm = TRUE)
-                sm2 <- paste("Merged from 1st dataframe:",
-                             merging.x, sep = " ")
-                missing.y <- sum(is.na(m.df$merge) | m.df$merge == 1)
-                sm3 <- paste("Missing from 2nd dataframe:",
-                             missing.y, sep = " ")
-                merging.y <- sum(m.df$merge == 2 | m.df$merge == 3,
-                                 na.rm = TRUE)
-                sm4 <- paste("Merged from 2nd dataframe:",
-                             merging.y, sep = " ")
-                missing.all <- sum(m.df$merge == 1 | m.df$merge == 2 |
-                                   is.na(m.df$merge))
-                sm5 <- paste("Total missing:",
-                             missing.all, sep = " ")
-                merging.all <- sum(m.df$merge == 3, na.rm = TRUE)
-                sm6 <- paste("Total merged:",
-                             merging.all, sep = " ")
-                cat(paste("Merging stats:", sm1, sm2, sm3, sm4, sm5, sm6,
-                          "", sep = "\n"))
+           }
+           if (show.stats == TRUE) {
+                    missing.x <- sum(is.na(m.df$merge) | m.df$merge == 2)
+                    sm1 <- paste("Missing from 1st dataframe:",
+                                 missing.x, sep = " ")
+                    merging.x <- sum(m.df$merge == 1 | m.df$merge == 3,
+                                     na.rm = TRUE)
+                    sm2 <- paste("Merged from 1st dataframe:",
+                                 merging.x, sep = " ")
+                    missing.y <- sum(is.na(m.df$merge) | m.df$merge == 1)
+                    sm3 <- paste("Missing from 2nd dataframe:",
+                                 missing.y, sep = " ")
+                    merging.y <- sum(m.df$merge == 2 | m.df$merge == 3,
+                                     na.rm = TRUE)
+                    sm4 <- paste("Merged from 2nd dataframe:",
+                                 merging.y, sep = " ")
+                    missing.all <- sum(m.df$merge == 1 | m.df$merge == 2 |
+                                       is.na(m.df$merge))
+                    sm5 <- paste("Total missing:",
+                                 missing.all, sep = " ")
+                    merging.all <- sum(m.df$merge == 3, na.rm = TRUE)
+                    sm6 <- paste("Total merged:",
+                                 merging.all, sep = " ")
+                    cat(paste("Merging stats:", sm1, sm2, sm3, sm4, sm5, sm6,
+                              "", sep = "\n"))
        }
        cat(paste("",
                  "df$merge variable meaning:",
--- test_script.R
+++ test_script.R
@@ -1,9 +1,13 @@
-devtools::install_github("newton-c/merge_stats_R")
-library(merge.stats)
-
+#devtools::install_github("newton-c/merge_stats_R")
+#library(merge.stats)
+source("R/merge_stats.R")
 
 dat.1 <- read.csv("~/Desktop/code_translations_r_package/ucdp-dyadic-201.csv")
 dat.2 <- read.csv("~/Desktop/code_translations_r_package/ucdp-prio-acd-201.csv")
 
+#dat.2$merge.y <- 3
+
 merge_df <- merge_stats(dat.1, dat.2, by = c("conflict_id", "year"),
                         show.stats = T)
+print(colnames(merge_df))
+rm(list = ls())
